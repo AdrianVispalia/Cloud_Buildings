fcdb40b23556fdeebe8809cfe0e28c73adaed60d
Adrián Vispalia Moreno <125102300+AdrianVispalia@users.noreply.github.com>
2024-02-07 15:35:50 +0100

    added db + cache to the azure container app deployment

--- rest_api/infrastructure/azure-container-apps/main.tf
+++ rest_api/infrastructure/azure-container-apps/main.tf
@@ -4,10 +4,57 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example_rg" {
   name     = "example-resources3"
-  location = "West Europe"
+  location = "North Europe"
 }
 
 
+resource "azurerm_redis_cache" "example_rc" {
+  name                     = "exampleredistest44"
+  resource_group_name      = azurerm_resource_group.example_rg.name
+  location                 = azurerm_resource_group.example_rg.location
+  capacity                 = 0
+  family                   = "C"
+  sku_name                 = "Basic"
+  enable_non_ssl_port      = false
+  minimum_tls_version      = "1.2"
+  redis_configuration {
+    maxmemory_policy = "volatile-lru"
+  }
+}
+
+
+resource "azurerm_postgresql_flexible_server" "example_ps" {
+  name                   = "example-postgres"
+  location               = azurerm_resource_group.example_rg.location
+  resource_group_name    = azurerm_resource_group.example_rg.name
+  sku_name               = "B_Standard_B1ms"
+  storage_mb             = 32768
+  version                = "11"
+  administrator_login    = "postgresql"
+  administrator_password = "password"
+  zone                   = "2"
+}
+
+resource "azurerm_postgresql_flexible_server_database" "example_pd" {
+  name                = "testdb"
+  server_id           = azurerm_postgresql_flexible_server.example_ps.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
+  name                = "AllowAzureServices"
+  server_id           = azurerm_postgresql_flexible_server.example_ps.id
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+
+// Containers
 resource "azurerm_container_registry" "example_cr" {
   name                     = "mynewexampleacr44"
   resource_group_name      = azurerm_resource_group.example_rg.name
@@ -39,7 +86,7 @@ resource "null_resource" "docker_build_push" {
   depends_on = [ azurerm_container_registry.example_cr ]
 }
 
-resource "azurerm_container_app_environment" "example_cap" {
+resource "azurerm_container_app_environment" "example_cae" {
   name                       = "Example-Environment3"
   location                   = azurerm_resource_group.example_rg.location
   resource_group_name        = azurerm_resource_group.example_rg.name
@@ -47,7 +94,7 @@ resource "azurerm_container_app_environment" "example_cap" {
 
 resource "azurerm_container_app" "example_ca" {
   name                         = "example-app"
-  container_app_environment_id = azurerm_container_app_environment.example_cap.id
+  container_app_environment_id = azurerm_container_app_environment.example_cae.id
   resource_group_name          = azurerm_resource_group.example_rg.name
   revision_mode                = "Single"
 
@@ -69,6 +116,7 @@ resource "azurerm_container_app" "example_ca" {
   ingress {
     external_enabled = true
     target_port = 80
+    allow_insecure_connections = true
     traffic_weight {
       percentage = 100
       latest_revision = true
@@ -87,7 +135,7 @@ resource "azurerm_container_app" "example_ca" {
         value = ""
       }
       env {
-          name  = "JWT_ALGORITH"
+          name  = "JWT_ALGORITHM"
           value = "HS256"
       }
       env {
@@ -96,30 +144,50 @@ resource "azurerm_container_app" "example_ca" {
       }
       env {
           name  = "DB_ENDPOINT"
-          value = "postgres:5432"
+          value = format(
+            "%s:%s",
+            azurerm_postgresql_flexible_server.example_ps.fqdn,
+            "5432"
+          )
       }
       env {
           name  = "DB_USER"
-          value = "user"
+          value = azurerm_postgresql_flexible_server.example_ps.administrator_login
       }
       env {
           name  = "DB_PASSWORD"
-          value = "password"
+          value = azurerm_postgresql_flexible_server.example_ps.administrator_password
       }
       env {
           name  = "DB_NAME"
-          value = "test_deb"
+          value = azurerm_postgresql_flexible_server_database.example_pd.name
       }
       env {
           name  = "REDIS_IP"
-          value = "redis"
+          value = azurerm_redis_cache.example_rc.hostname
       }
       env {
           name  = "REDIS_PORT"
-          value = "6379"
+          value = azurerm_redis_cache.example_rc.port
       }
     }
   }
 
   depends_on = [ null_resource.docker_build_push ]
+}
+
+output "redis_hostname" {
+  value = azurerm_redis_cache.example_rc.hostname
+}
+
+output "is_database_public" {
+  value = azurerm_postgresql_flexible_server.example_ps.public_network_access_enabled
+}
+
+output "postgres_endpoint" {
+  value = format(
+            "%s:%s",
+            azurerm_postgresql_flexible_server.example_ps.fqdn,
+            "5432"
+          )
 }
